200f50bc8cfca6f05c17492c687b6bc7bdbf9912
cheungchikting <cheungchikting@gmail.com>
2021-04-16 11:59:26 +0800

    commit

--- js/clear.js
+++
@@ -1,4 +0,0 @@
-clearButton.addEventListener('click', function () {
-    contextReal.clearRect(0, 0, canvasReal.width, canvasReal.height);
-    contextDraft.clearRect(0, 0, canvasReal.width, canvasReal.height);
-})
